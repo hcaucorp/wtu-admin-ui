075998a3fe172e0b99558c66c655fe4414ed5812
Hubert Czerpak <hubert.czerpak@gmail.com>
2018-12-25 21:34:06 +0000

    renaming main project to voucher-shop-ui

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to cgc-manager-ui!');
+    expect(page.getTitleText()).toEqual('Welcome to voucher-shop-ui!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cgc-manager-ui'`, () => {
+  it(`should have as title 'voucher-shop-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cgc-manager-ui');
+    expect(app.title).toEqual('voucher-shop-ui');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cgc-manager-ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to voucher-shop-ui!');
   });
 });
