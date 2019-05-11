335daf34010a99253d5176e36e4a14e4b58d5c97
Hubert Czerpak <hubert.czerpak@gmail.com>
2019-05-11 21:24:04 +0100

    refresh on wallet list

--- src/app/wallets/wallet-list/wallet-list.component.ts
+++ src/app/wallets/wallet-list/wallet-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Wallet } from '../model/wallet';
-import { Observable } from 'rxjs';
+import { Observable, interval } from 'rxjs';
 import { LoadWalletsAction } from '../actions/wallet.actions';
 import { WalletsFeatureState, getWalletsState } from '../reducers/wallet.reducer';
-import { map } from 'rxjs/operators';
+import { map, window, takeWhile } from 'rxjs/operators';
 import { MatDialog } from '@angular/material';
 import { QrComponent } from 'src/app/qr/qr-dialog/qr-dialog.component';
 
@@ -16,18 +16,29 @@ import { QrComponent } from 'src/app/qr/qr-dialog/qr-dialog.component';
 export class WalletListComponent implements OnInit {
 
   wallets$: Observable<Wallet[]>;
-
   state$: Observable<WalletsFeatureState>;
+  refreshSeconds$: Observable<number>;
+  isActive = true;
 
   constructor(private store: Store<any>,
     private dialog: MatDialog) {
     this.state$ = store.select<WalletsFeatureState>(getWalletsState);
     this.wallets$ = this.state$.pipe(
-      map(state => state.wallets));
+      map(state => state.wallets)
+    );
+    this.refreshSeconds$ = interval(1000);
   }
 
   ngOnInit() {
     this.store.dispatch(new LoadWalletsAction());
+    this.refreshSeconds$
+      .pipe(
+        window(interval(60 * 1000)),
+        takeWhile(_ => this.isActive)
+      )
+      .subscribe(_ => {
+        this.store.dispatch(new LoadWalletsAction());
+      });
   }
 
   showQR(qrdata: string) {
